7fcd5782234dcea26d4f1ceaf04e13f86f98cc7e
Slava Korolev <vyacheslav.korolev.ru@mail.ru>
2024-02-04 15:30:21 +0700

    added first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1. Совместная работа
+// Семинар 2. Простые алгоритмы на C#
+// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+int number = 678;
+int firstDigit = number / 100; 
+int  thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine($"В числе {number} => {result} ");
